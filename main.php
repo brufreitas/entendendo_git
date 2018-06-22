cec7f66606cc912f30c196009c46ec5bd0696867
Bruno Freitas <bfreitas@uranet.com.br>
2018-06-22 11:16:46 -0300

    Dúvida: foi feito o commit?

--- main.php
+++ main.php
@@ -3,7 +3,8 @@ function entendendo() {}
   return
     "Um dia eu entenderei o git\n".
     // "Ou não".
-    "Tá quase!".
+    // "Tá quase!".
+    "Fiquei na dúvida denovo. Como sei que foi feito o commit mas não o push?".
     ;
 }
 
